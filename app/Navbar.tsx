c32b4a799865b25daae10a41c4ac27e5fdb71085
Korbin de Man <korbindeman4@gmail.com>
2023-10-02 00:58:13 +0200

    feat: updated navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,31 +1,27 @@
-import Link from "next/link";
-
-function Tab() {
-  return <div className="rounded-md bg-gray-200 px-4 py-2">Tab</div>;
-}
+import { BellIcon, Cog6ToothIcon } from "@heroicons/react/20/solid";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between border-b p-4">
-      <div className="flex items-center gap-2">
-        <Link href="/">OGSM</Link>
-        <ul className="flex gap-2">
-          <Tab />
-          <Tab />
-          <Tab />
-        </ul>
+    <nav className="flex items-center justify-between border-b p-3">
+      <div className="w-1/3"></div>
+      <div className="w-1/3">
+        <input
+          type="text"
+          className="w-full rounded border px-2 py-1 text-sm"
+          placeholder="Search..."
+        />
       </div>
-      <ul className="flex gap-2">
-        <li className="">
-          <Link href="/">Profile</Link>
-        </li>
-        <li className="">
-          <Link href="/">Test</Link>
-        </li>
-        <li className="">
-          <Link href="/">Settings</Link>
-        </li>
-      </ul>
+      <div className="flex w-1/3 justify-end gap-2">
+        <div className="relative cursor-pointer rounded-full bg-gray-100 p-2 transition hover:bg-gray-200">
+          <BellIcon className="h-5 w-5 text-gray-500" />
+          <span className="absolute right-0 top-0 inline-flex -translate-y-1/4 translate-x-1/4 transform items-center justify-center rounded-full bg-red-600 px-1.5 py-1 text-xs font-bold leading-none text-red-100">
+            2
+          </span>
+        </div>
+        <div className="cursor-pointer rounded-full bg-gray-100 p-2 transition hover:bg-gray-200">
+          <Cog6ToothIcon className="h-5 w-5 text-gray-500" />
+        </div>
+      </div>
     </nav>
   );
 }
