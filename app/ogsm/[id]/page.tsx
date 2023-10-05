7063864bdd0adc374bf804e90d198327bbb4222a
Korbin de Man <korbindeman4@gmail.com>
2023-10-05 18:47:04 +0200

    feat: started ogsm page implementation

--- app/ogsm/[id]/page.tsx
+++ app/ogsm/[id]/page.tsx
@@ -2,11 +2,11 @@ import Card from "@/components/Card";
 
 export default function Ogsm() {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="col-span-3">
+    <div className="">
+      <div className="mb-2">
         <Card>
           <div className="flex items-center gap-2">
-            <span className="text-sm text-gray-600">Objective:</span>
+            <h2 className="text-sm text-gray-600">Objective:</h2>
             <input
               type="text"
               className="w-full rounded border px-2 py-1 text-sm"
@@ -14,7 +14,134 @@ export default function Ogsm() {
           </div>
         </Card>
       </div>
-      <Card></Card> <Card></Card> <Card></Card>
+      <div className="flex gap-2">
+        <div className="w-80">
+          <Card>
+            <h2 className="text-sm text-gray-600">Goals:</h2>
+            <ul className="ml-6 list-disc">
+              <li>Random thing</li>
+              <li>Other thing</li>
+              <li>Cool thing</li>
+            </ul>
+          </Card>
+        </div>
+        <div className="flex grow flex-col gap-2">
+          <Card>
+            <div className="flex text-sm">
+              <div className="grow border-r">
+                <div className="">
+                  <h3 className="">Lorem ipsum</h3>
+                  <ul className="ml-6 list-disc">
+                    <li>Random thing</li>
+                    <li>Other thing</li>
+                    <li>Cool thing</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="grow border-r">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+              <div className="grow">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div className="flex text-sm">
+              <div className="grow border-r">
+                <div className="">
+                  <h3 className="">Lorem ipsum</h3>
+                  <ul className="ml-6 list-disc">
+                    <li>Random thing</li>
+                    <li>Other thing</li>
+                    <li>Cool thing</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="grow border-r">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+              <div className="grow">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div className="flex text-sm">
+              <div className="grow border-r">
+                <div className="">
+                  <h3 className="">Lorem ipsum</h3>
+                  <ul className="ml-6 list-disc">
+                    <li>Random thing</li>
+                    <li>Other thing</li>
+                    <li>Cool thing</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="grow border-r">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+              <div className="grow">
+                <ul>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia, reprehenderit?
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,5 +2,7 @@ import { ReactNode } from "react";
 
 type Props = { children?: ReactNode };
 export default function Card({ children }: Props) {
-  return <div className="rounded-lg border bg-white p-8">{children}</div>;
+  return (
+    <div className="h-full rounded-lg border bg-white p-8">{children}</div>
+  );
 }
