9ff438b06ded82bee091d57fd0cd81079b40b01d
mrcoldshower <pablosanderman@gmail.com>
2023-10-11 15:15:36 +0200

    feat: dashboard connected to database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { prisma } from "@/lib/prisma";
 import { StarIcon } from "@heroicons/react/20/solid";
+import { Ogsm } from "@prisma/client";
 import Link from "next/link";
 
-function ProjectCard() {
+interface ProjectCardProps {
+  ogsm: Ogsm;
+}
+
+function ProjectCard({ ogsm }: ProjectCardProps) {
   return (
     <Link
       href="/ogsm/1"
@@ -9,10 +15,8 @@ function ProjectCard() {
     >
       <div className="mb-14 flex justify-between">
         <div className="">
-          <h2>Example Title</h2>
-          <p className="text-sm text-gray-500">
-            Example subtitle for this project.
-          </p>
+          <h2>{ogsm.title}</h2>
+          <p className="text-sm text-gray-500">{ogsm.objective}</p>
         </div>
         <div className="p-1 text-yellow-400 transition hover:text-yellow-500">
           <StarIcon className="h-5 w-5" />
@@ -31,10 +35,18 @@ function ProjectCard() {
   );
 }
 
-export default function Dashboard() {
+async function getData() {
+  const ogsms = await prisma.ogsm.findMany();
+  return ogsms;
+}
+
+export default async function Dashboard() {
+  const data = await getData();
   return (
     <main className="grid grid-cols-3 gap-4">
-      <ProjectCard />
+      {data.map((ogsm) => (
+        <ProjectCard ogsm={ogsm} key={ogsm.id} />
+      ))}
     </main>
   );
 }
