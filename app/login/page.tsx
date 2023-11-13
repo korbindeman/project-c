9db498df0e1629b981583470490a49de185d751d
schtormm <storm.noordermeer@gmail.com>
2023-11-13 09:50:39 +0100

    misc: added comments and tooltips

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function login() {
+  return (
+    // keeping space for future login component from provider
+    <div className="">
+      <div className="h-96 rounded-md border-2 text-center text-lg font-bold">
+        Please log in
+      </div>
+    </div>
+  );
+}
