c0bc4b96b39ade4df7503bc1ae903db4b7120add
Korbin de Man <korbindeman4@gmail.com>
2023-12-13 10:20:39 +0100

    style: formatting

--- app/api/ogsm/strategy/action/route.ts
+++ app/api/ogsm/strategy/action/route.ts
@@ -3,19 +3,18 @@ import { Action } from "@prisma/client";
 import { NextRequest } from "next/server";
 
 export async function PUT(request: NextRequest) {
-    // TODO: make this error if no  strategies are given
-    const actionsUpdated: Action[] = await request.json();
-    actionsUpdated.forEach(async (action: Action) => {
-        await prisma.action.update({
-            where: {
-                id: action.id,
-            },
-            data: {
-                content: action.content,
-
-            },
-        });
+  // TODO: make this error if no  strategies are given
+  const actionsUpdated: Action[] = await request.json();
+  actionsUpdated.forEach(async (action: Action) => {
+    await prisma.action.update({
+      where: {
+        id: action.id,
+      },
+      data: {
+        content: action.content,
+      },
     });
+  });
 
-    return Response.json({ actionsUpdated });
+  return Response.json({ actionsUpdated });
 }
