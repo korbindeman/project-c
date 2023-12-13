5657fe18161f901d4143429ce23fdb438d1559fd
Korbin de Man <korbindeman4@gmail.com>
2023-12-13 10:18:39 +0100

    feat: add Toaster to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
@@ -25,6 +26,7 @@ export default function RootLayout({
         > */}
         <Navbar />
         {children}
+        <Toaster />
         {/* </ThemeProvider> */}
       </body>
     </html>
